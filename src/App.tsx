8ffcb17eebfd4b4d8e03b5e0a777b1824a46778e
JasonAo <jasonao4869@gmail.com>
2025-03-11 02:21:37 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,27 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-black text-white min-h-screen">
-      
+    <div className="bg-black text-white min-h-screen flex flex-col">
+      <header className={`transition duration-300 ease-in-out ${isScrolled ? "bg-gray-700" : "bg-gray-900"} p-4 fixed w-full z-10`}>
+        <p className='text-3xl text-white'>Start Here</p>
+      </header>
+      <div className="flex-1 pt-16">
+        <section className="p-4">
+          <h2 className='text-lg font-bold'>工作流程</h2>
+          <ul className='list-disc pl-16'>
+            <li></li>
+          </ul>
+        </section>
+        <section className="p-4">
+        <h2 className='text-lg font-bold'>工作流程</h2>
+          <ul className='list-disc pl-16'>
+            <li></li>
+          </ul>
+        </section>
+      </div>
+      <footer className="bg-gray-900 p-4 mt-4">
+        <p>©Copyright</p>
+      </footer>
     </div>
   );
 }
