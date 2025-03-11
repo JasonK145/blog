d9e7b394ec138c2ff3bbc069bca6bde957ed2fbe
JasonAo <jasonao4869@gmail.com>
2025-03-12 03:47:26 +0800

    update the target
    fixed the target

--- src/App.tsx
+++ src/App.tsx
@@ -19,22 +19,38 @@ function App() {
       <header className={`fixed top-0 left-0 right-0 transition duration-300 ease-in-out ${isScrolled ? "bg-gray-500" : "bg-gray-100"} p-4 flex w-full z-10 items-center justify-between`}>
         <p className='text-3xl'>Start Here</p>
       </header>
-      <div className="flex-1 pt-20">
+      <div className="flex-1 pt-20 mt-7">
         <section className="p-4">
           <p className='text-2xl font-bold'>目標</p>
           <ul className='list-disc pl-16 text-lg'>
-            <li></li>
+            <li>前端八股文題目學習</li>
+            <li>制作一個可以用於使用的個人blog</li>
+            <li>制作一個可以用於交流的網站</li>
+            <li>將含有以上兩點功能的網站制作成一個app</li>
           </ul>
         </section>
         <section className="p-4">
-          <p className='text-2xl font-bold'>流程</p>
+          <p className='text-2xl font-bold'>前端需要學習內容</p>
           <ul className='list-disc pl-16 text-lg'>
-            <li></li>
+            <li>垃圾回收機制 - 引用計數算法 和 標記清除算法 - link</li>
+          </ul>
+        </section>
+        <section className="p-4">
+          <p className='text-2xl font-bold'>Blog</p>
+          <ul className='list-disc pl-16 text-lg'>
+            <li>個人blog</li>
+          </ul>
+        </section>
+        <section className="p-4">
+          <p className='text-2xl font-bold'>交流網站</p>
+          <ul className='list-disc pl-16 text-lg'>
+            <li>Website</li>
+            <li>App</li>
           </ul>
         </section>
         <Button>Antdesign test</Button>
       </div>
-      <footer className="bg-gray-900 p-4 mt-4 text-white">
+      <footer className="bg-gray-500 p-4 mt-4 text-white">
         <p>©Copyright</p>
       </footer>
     </div>
